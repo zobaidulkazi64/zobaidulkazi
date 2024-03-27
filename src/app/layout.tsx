11cebbae932af548d4d05dd1b00e179fd479d286
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-03-27 23:56:53 +0600

    add~favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { MetaData } from "@/config/MetaData";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,24 +21,24 @@ export default function RootLayout({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/public/apple-touch-icon.png"
+          href={MetaData.faviconLinkAppleTouchIcon}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/public/favicon-32x32.png"
+          href={MetaData.faviconLink32x32}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/public/favicon-16x16.png"
+          href={MetaData.faviconLink16x16}
         />
         <link rel="manifest" href="/public/site.webmanifest"></link>
         <link
           rel="mask-icon"
-          href="/public/safari-pinned-tab.svg"
+          href={MetaData.androidChromeLink512x512}
           color="#5bbad5"
         />
         <meta name="msapplication-TileColor" content="#da532c" />
---
+++ src/config/MetaData.ts
@@ -0,0 +1,12 @@
+export const MetaData = {
+  faviconLink32x32:
+    "https://github.com/zobaidulkazi64/zobaidulkazi/blob/main/public/favicon-32x32.png?raw=true",
+  faviconLink16x16:
+    "https://github.com/zobaidulkazi64/zobaidulkazi/blob/main/public/favicon-16x16.png?raw=true",
+  faviconLinkAppleTouchIcon:
+    "https://github.com/zobaidulkazi64/zobaidulkazi/blob/main/public/apple-touch-icon.png?raw=true",
+  androidChromeLink192x192:
+    "https://github.com/zobaidulkazi64/zobaidulkazi/blob/main/public/android-chrome-192x192.png?raw=true",
+  androidChromeLink512x512:
+    "https://github.com/zobaidulkazi64/zobaidulkazi/blob/main/public/android-chrome-512x512.png?raw=true",
+};
