34ed95758ae0598f09304c0174e20d36039011df
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-06-01 23:47:08 +0600

    skills data added

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SkillsPage = () => {
+  return (
+    <>
+      <h1>Skills Page</h1>
+    </>
+  );
+};
+
+export default SkillsPage;
---
+++ src/config/SkillsPageData.ts
@@ -0,0 +1,63 @@
+const SkillsPageData = [
+  {
+    name: "HTML",
+    icon: "html",
+  },
+  {
+    name: "CSS",
+    icon: "css",
+  },
+  {
+    name: "JavaScript",
+    icon: "javascript",
+  },
+  {
+    name: "TypeScript",
+    icon: "typescript",
+  },
+  {
+    name: "React",
+    icon: "react",
+  },
+  {
+    name: "Next.js",
+    icon: "nextjs",
+  },
+  {
+    name: "Tailwind",
+    icon: "tailwind",
+  },
+  {
+    name: "Git",
+    icon: "git",
+  },
+  {
+    name: "GitHub",
+    icon: "github",
+  },
+  {
+    name: "Figma",
+    icon: "figma",
+  },
+  {
+    name: "Linux",
+    icon: "linux",
+  },
+  {
+    name: "C",
+    icon: "c",
+  },
+  {
+    name: "C++",
+    icon: "cpp",
+  },
+  {
+    name: "C#",
+    icon: "csharp",
+  },
+  {
+    name: "Python",
+    icon: "python",
+  },
+];
+export default SkillsPageData;
