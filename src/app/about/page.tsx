c15d229f97138b4291233ae36c326c1a8b5876f0
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-06-01 23:57:45 +0600

    About page added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,12 @@
+import About from "@/components/about/About";
+import ThemeToggleButton from "@/components/themes/ThemeToggleButton";
 import React from "react";
 
 const AboutPage = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <About />
+      <ThemeToggleButton />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
+import Skills from "@/components/skills/Skills";
 
 function Home() {
   return (
     <>
       <Header />
       <Hero />
+
+      <Skills />
     </>
   );
 }
